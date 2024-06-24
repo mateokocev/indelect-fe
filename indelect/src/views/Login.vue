5cacfc7067289687f0a9f273f276bdf3a8180c29
mateokocev <mkocev@student.unipu.hr>
2024-06-24 20:47:06 +0200

    pronadena greska na kraju haha

--- indelect/src/views/Login.vue
+++ indelect/src/views/Login.vue
@@ -226,46 +226,48 @@ export default {
     };
 
     const login = async () => {
-  showError.value = false;
-  showEmpty.value = false;
+      showError.value = false;
+      showEmpty.value = false;
 
-  if (!email.value || !password.value) {
-    console.error("All fields are required");
-    showEmpty.value = true;
-    return;
-  }
+      if (!email.value || !password.value) {
+        console.error("All fields are required");
+        showEmpty.value = true;
+        return;
+      }
 
-  isLoading.value = true;
+      isLoading.value = true;
 
-  try {
-    const hashedPassword = await hashPassword(password.value);
+      try {
+        const hashedPassword = await hashPassword(password.value);
 
-    const response = await axios.post("/login", {
-      email: email.value,
-      password: hashedPassword,
-    });
+        const response = await axios.post("/login", {
+          email: email.value,
+          password: hashedPassword,
+        });
 
-    console.log("Login successful:", response.data);
-    localStorage.setItem("token", response.data.token);
-    localStorage.setItem("isAdmin", response.data.isAdmin);
-    localStorage.setItem("userEmail", email.value); // Save email address locally
-    piniaStorage.setAuthData(response.data.token, response.data.isAdmin);
+        console.log("Login successful:", response.data);
+        localStorage.setItem("token", response.data.token);
+        localStorage.setItem("isAdmin", response.data.isAdmin);
+        localStorage.setItem("userEmail", email.value); // Save email address locally
+        piniaStorage.setAuthData(response.data.token, response.data.isAdmin);
 
-    if (piniaStorage.getAdmin && isMobile.value) {
-      router.push({ name: 'warning' });
-    } else if (!piniaStorage.getAdmin && !isMobile.value) {
-      router.push({ name: 'warning' });
-    } else if (piniaStorage.getAdmin && !isMobile.value) {
-      router.push({ name: 'cmshome' });
-    }
+        if (piniaStorage.getAdmin && isMobile.value) {
+          router.push({ name: 'warning' });
+        } else if (!piniaStorage.getAdmin && !isMobile.value) {
+          router.push({ name: 'warning' });
+        } else if (piniaStorage.getAdmin && !isMobile.value) {
+          router.push({ name: 'cmshome' });
+        } else if (!piniaStorage.getAdmin && isMobile.value) {
+          router.push({ name: 'ticketlist' });
+        }
 
-  } catch (error) {
-    console.error("Login failed:", error);
-    showError.value = true;
-  } finally {
-    isLoading.value = false;
-  }
-};
+      } catch (error) {
+        console.error("Login failed:", error);
+        showError.value = true;
+      } finally {
+        isLoading.value = false;
+      }
+    };
 
     return {
       isMobile,
