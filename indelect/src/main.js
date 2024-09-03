25079cf60a4b48e4b64bdbe307efbc1a93fb4527
mateokocev <mkocev@student.unipu.hr>
2024-09-03 19:21:42 +0200

    mali ispravak

--- indelect/src/main.js
+++ indelect/src/main.js
@@ -9,7 +9,7 @@ import * as directives from 'vuetify/directives'
 import axios from 'axios'
 import { PiniaVuePlugin, createPinia } from 'pinia'
 // axios.defaults.baseURL = "http://localhost:3000/api";
-axios.defaults.baseURL = "https://indelect-be.onrender.com/10000/api"
+axios.defaults.baseURL = "https://indelect-be.onrender.com/api"
 
 const pinia = createPinia()
 const app = createApp(App)
