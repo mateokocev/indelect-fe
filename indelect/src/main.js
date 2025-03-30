fd6a391e1c51760d67a96f4dc781ba99dbab06d9
Mateo Kocev <mkocev@student.unipu.hr>
2025-03-30 12:43:14 +0200

    its gonna work
    hehhehe

--- indelect/src/main.js
+++ indelect/src/main.js
@@ -11,8 +11,13 @@ import { PiniaVuePlugin, createPinia } from 'pinia'
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
  axios.defaults.baseURL = "http://localhost:3000/api";
 //axios.defaults.baseURL = "https://indelect-be.onrender.com/api"
+=======
+axios.defaults.baseURL = "http://localhost:3000/api";
+// axios.defaults.baseURL = "https://indelect-be.onrender.com/api"
+>>>>>>> Stashed changes
 =======
 axios.defaults.baseURL = "http://localhost:3000/api";
 // axios.defaults.baseURL = "https://indelect-be.onrender.com/api"
